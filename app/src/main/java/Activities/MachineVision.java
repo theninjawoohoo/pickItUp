4331c1a3f6cd102fc5211de31610dbf4df43c6e4
Andy <wuandyw@sfu.ca>
2020-01-12 11:01:53 -0800

    Machine BUgged

--- app/src/main/java/Activities/MachineVision.java
+++ app/src/main/java/Activities/MachineVision.java
@@ -11,18 +11,30 @@ import android.widget.ImageButton;
 import android.widget.Toast;
 
 import com.example.myapplication.R;
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.BatchAnnotateImagesResponse;
+import com.google.cloud.vision.v1.EntityAnnotation;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.protobuf.ByteString;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import Models.UserSingleton;
-import Models.detectLabels;
 
 public class MachineVision extends AppCompatActivity {
 
     private String pStream = "placeholder";
-    private static final String TAG = "MachineActivity";
+    private final String TAG = "MachineActivity";
+    private List<String> listOfLabels = new ArrayList<>();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,16 +50,46 @@ public class MachineVision extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 try {
-                    detectLabels.detectLabels(filePath);
+                    detectLabels(filePath);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                List<String> listOfLabels = detectLabels.getLabels();
-                Log.d(TAG, "onClick: " + listOfLabels.get(0));
-                Toast.makeText(MachineVision.this, listOfLabels.get(0), Toast.LENGTH_SHORT).show();
+
             }
         });
     }
 
+    public void detectLabels(String filePath) throws Exception {
+        List<AnnotateImageRequest> requests = new ArrayList<>();
+
+        ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
+
+        Image img = Image.newBuilder().setContent(imgBytes).build();
+        Feature feat = Feature.newBuilder().setType(Feature.Type.LABEL_DETECTION).build();
+        AnnotateImageRequest request =
+                AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+        requests.add(request);
+
+        try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+            BatchAnnotateImagesResponse response = client.batchAnnotateImages(requests);
+            List<AnnotateImageResponse> responses = response.getResponsesList();
+
+            for (AnnotateImageResponse res : responses) {
+                final float THRESHOLD = 0f;
+
+                // For full list of available annotations, see http://g.co/cloud/vision/docs
+                for (EntityAnnotation annotation : res.getLabelAnnotationsList()) {
+                    // only place confidence level over threshold into labels
+                    annotation.getAllFields().forEach((k, v) -> Log.d("annotation", String.format("%s : %s\n", k, v.toString())));
+                    String tag = annotation.getDescription();
+                    float confidence = annotation.getScore();
+
+                    if (confidence > THRESHOLD) {
+                        listOfLabels.add(tag);
+                    }
+                }
+            }
+        }
+    }
 
 }
