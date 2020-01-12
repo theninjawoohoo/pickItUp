ebd99917c06a3f97907ab694af6133cc4f287735
arty <art_yang@sfu.ca>
2020-01-11 19:49:57 -0800

    added new ImageIdentifier class

---
+++ app/src/main/java/ImageIdentifier.java
@@ -0,0 +1,12 @@
+import com.google.cloud.vision.v1.ImageAnnotationContext;
+import com.google.cloud.vision.v1.Image;
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+
+public class ImageIdentifier {
+    public static void main(String... args) throws Exception {
+        try (ImageAnnotatorClient vision = ImageAnnotatorClient.create()) {
+
+        }
+
+    }
+}
